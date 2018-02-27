a346efadb192ecfd87eef56fb69d5eef4cf4c412
ssaegert <ssaegert@gmail.com>
2018-02-27 10:56:16 -0600

    Completed first Hello World project

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void noPrint(String[] args) {
+        System.out.print("no print");
+    }
+}
