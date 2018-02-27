f00042ed11e9ef2ddf74ca686ed9d1e4e68d6c56
ssaegert <ssaegert@gmail.com>
2018-02-27 12:21:21 -0600

    Completed Syntax, Data Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,35 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
-    public static void noPrint(String[] args) {
-        System.out.print("no print");
-    }
-    public static void noprinttwo(String[] args) {
-        System.out.print("no print two");
+
+        System.out.print("no break,");
+
+        System.out.println(" continued line");
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Scott Saegert";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        int y = 4;
+        System.out.println(y += 5);
+
+        int a = 3;
+        int b = 4;
+        System.out.println(b *= a);
+
+        int c = 10;
+        int d = 2;
+        System.out.println(c /= d);
+        System.out.println(d -= c);
+
     }
 }
