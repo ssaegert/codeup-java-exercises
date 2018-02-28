f82df1ec0a3d43a0d8b6601bdf5bfd5ba61bf6a7
ssaegert <ssaegert@gmail.com>
2018-02-28 10:35:37 -0600

    completed Control Flow exercises 1 and 2 and started 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+        //Exercise 1A
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //Exercise 1B
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        }while(i <= 100);
+
+//        int i = 2;
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        }while(i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        }while(i <= 1000000);
+
+
+        //Exercise 1C
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 2; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+
+        //Exercise 2
+//        for(int i = 1; i <= 100; i++) {
+//            if(i % 3 == 0) {
+//                if (i % 3 == 0 && i % 5 == 0){
+//                    System.out.println("FizzBuzz");
+//                    continue;
+//                } else {
+//                System.out.println("Fizz");
+//                continue;
+//                }
+//            } else if(i % 5 == 0) {
+//                if (i % 3 == 0 && i % 5 == 0){
+//                    System.out.println("FizzBuzz");
+//                    continue;
+//                } else {
+//                System.out.println("Buzz");
+//                continue;
+//                }
+//            }
+//            System.out.println(i);
+//        }
+        //Exercise 3
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What number would you like to go up to?");
+        int userNumber = sc.nextInt();
+        System.out.println(userNumber);
+
+        System.out.println("\nHere is your table!");
+        System.out.println("\nnumber | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 1; i <= userNumber; i += 1) {
+        String output = "";
+        output += String.format("%-7d", i);
+        output += String.format("|%-9d", i * i);
+        output += String.format("|%d", i * i * i);
+        System.out.println(output);
+                }
+
+    }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -34,38 +32,6 @@ public class HelloWorld {
         System.out.println(d -= c);
 
 
-        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter a number: ");
-//        int userNumber = sc.nextInt();
-//        System.out.println("You entered: -- " + userNumber + " --");
-//--------------------------------
-//        System.out.println("Type Three Words");
-//        String wordOne, wordTwo, wordThree;
-//
-//        wordOne = sc.nextLine();
-//        wordTwo = sc.nextLine();
-//        wordThree = sc.nextLine();
-//
-//        System.out.println(wordOne);
-//        System.out.println(wordTwo);
-//        System.out.println(wordThree);
-//--------------------------------
-//        System.out.println("Type a sentence");
-//        String sentence;
-//        sentence = sc.nextLine();
-//        System.out.println(sentence);
-//--------------------------------
-        System.out.println("Type the classroom Width and Length");
-        String classWidth, classLength;
-        classWidth = sc.nextLine();
-        classLength = sc.nextLine();
-        double width = Double.parseDouble(classWidth);
-        double length = Double.parseDouble(classLength);
-        double area = width * length;
-        double perimeter = 2 * width + 2 * length;
-
-        System.out.println("The classroom area is: " + area);
-        System.out.println("The classroom perimeter is: " + perimeter);
 
     }
 }
