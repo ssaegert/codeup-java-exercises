144dbaef0547690ef48ef5efaa51afacd14a4ff3
ssaegert <ssaegert@gmail.com>
2018-03-02 11:10:21 -0600

    completed Burgers mini exercise

---
+++ src/BurgerTester.java
@@ -0,0 +1,14 @@
+public class BurgerTester {
+    public static void main(String[] args) {
+
+        BurgerTools.mostPopularTopping = "pickles";
+        BurgerTools.averageDaysBeforeExpiration = 5;
+        BurgerTools.temperatureWhenCooked = 116;
+
+        System.out.println(BurgerTools.mostPopularTopping);
+        System.out.println(BurgerTools.averageDaysBeforeExpiration);
+        System.out.println(BurgerTools.temperatureWhenCooked);
+        BurgerTools.grill();
+
+    }
+}
